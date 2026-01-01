6d166c7172e7d0f09002e21c979cb009ed969f8d
Debayan Lahiry <debayan.lahiry@gmail.com>
2026-01-02 04:19:56 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,16 @@
-var typed = new Typed(".multiple-text",{
-    strings : ["Cybersecurity Student." , "Web Developer." , "Ethical Hacker."],
-    typespeed: 100,
-    backspeed: 100,
-    backdelay: 1000,
+var typed = new Typed(".multiple-text", {
+    strings: ["Cybersecurity Student.", "Web Developer.", "Ethical Hacker."],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
     loop: true
-})
+});
 
+// Mobile menu toggle
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
